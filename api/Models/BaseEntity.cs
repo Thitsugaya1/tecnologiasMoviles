046e0c617255b9802dc82e902a88e3219b88deff
DarkNacho <dark_nacho_xd@hotmail.cl>
2020-07-08 14:50:55 -0400

    Set Default Values

--- api/Models/BaseEntity.cs
+++ api/Models/BaseEntity.cs
@@ -4,9 +4,8 @@ namespace TecnologiasMovilesApi.Models
 {
     public class BaseEntity
     {
-        public long Id { get; set; }
-        public DateTime Created { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
         public DateTime? Deleted { get; set; } = null;
-        public bool IsDeleted { get; set; }
+        public bool IsDeleted { get; set; } = false;
     }
 }
