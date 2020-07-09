db253decb27214e67bea3321247458bf87d01daa
gbunny <gbunny@protonmail.com>
2020-07-09 13:48:42 -0400

    Admin Ticket list

---
+++ flutterApp/lib/ui/admin/adminPage.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:ticketapp/ui/admin/adminTicketList.dart';
+
+class AdminMainpage extends StatefulWidget {
+  AdminMainpage({Key key}) : super(key: key);
+
+  @override
+  _AdminMainpageState createState() => _AdminMainpageState();
+}
+
+class _AdminMainpageState extends State<AdminMainpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+       title: Text("Tickets"),
+        actions: <Widget>[
+          Container(
+            height: 40,
+            width: 40,
+            child: Icon(Icons.replay),
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: Icon(Icons.add)
+          )
+        ],
+      ),
+      body: Center(
+        child: AdminTicketList(),
+      ),
+    );
+  }
+}
