9a4e17f8e3e89de30a655d53dc01cd5270834a85
gbunny <gbunny@protonmail.com>
2020-07-21 15:29:06 -0400

    Success Alert with Callback

--- flutterApp/lib/ui/Utilities.dart
+++ flutterApp/lib/ui/Utilities.dart
@@ -18,5 +18,23 @@ class Utilities {
     );
   } 
 
+  static AlertDialog successAlert(Function() callback){
+    return AlertDialog(
+      content: SingleChildScrollView(
+        child: ListBody(
+          children: <Widget>[
+            Text("Success")
+          ],
+        )
+      ),
+      actions: <Widget>[
+        RaisedButton(
+          child: Text("OK"),
+          onPressed: callback,
+        )
+      ],
+    );
+  }
+
 
 }
