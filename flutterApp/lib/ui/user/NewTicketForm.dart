fde6c47f3b47863bc85e8417cfd33432fc385132
Alisboa13 <alisboa@alumnos.utalca.cl>
2020-07-30 01:19:03 -0400

    Updated Form

--- flutterApp/lib/ui/user/NewTicketForm.dart
+++ flutterApp/lib/ui/user/NewTicketForm.dart
@@ -26,14 +26,14 @@ class _NewTicketFormState extends State<NewTicketForm> {
 
   final initialDateController = TextEditingController();
   final initialTimeOfDayController = TextEditingController();
-  final endDateController =  TextEditingController();
+  final endDateController = TextEditingController();
   final endTimeOfDayController = TextEditingController();
   final directionTextController = TextEditingController();
 
-  String dateText(DateTime date){
-    if(date != null){
+  String dateText(DateTime date) {
+    if (date != null) {
       return date.month.toString() + "/" + date.day.toString();
-    }else{
+    } else {
       return "MM/DD";
     }
   }
@@ -41,103 +41,70 @@ class _NewTicketFormState extends State<NewTicketForm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pedir Ticket')
-      ),
+      appBar: AppBar(title: Text('Pedir Ticket')),
       body: Center(
-        child: SingleChildScrollView( child: _pedirTicketForm(context)),
+        child: SingleChildScrollView(child: _pedirTicketForm(context)),
       ),
     );
   }
 
-  Widget _pedirTicketForm(BuildContext context){
+  Widget _pedirTicketForm(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(10),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Text('Descripcion'),
-          /* TextFormField(
-            decoration: InputDecoration(
-                icon: Icon(Icons.person),
-                hintText: "Email",
-                labelText: "Email",
-                ),
-                autofocus: true,
-            controller: null,
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              icon: Icon(Icons.person),
-              hintText: "Nombre",
-              labelText: "Nombre"
-            )
-          ),
-          TextFormField(
-            decoration: InputDecoration( 
-              icon: Icon(Icons.person),
-              hintText: "Apellido",
-              labelText: "Apellido"
-            )
-          ), */
-          Text('Direccion'),
-          TextFormField(
-            decoration: InputDecoration( 
+      child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+        TextFormField(
+          decoration: InputDecoration(
               icon: Icon(Icons.place),
               hintText: "Direccion",
-              labelText: "Direccion"
-            ),
-            onTap: getGeo,
-            controller: directionTextController,
-          ),
-          Text('Fecha'),
-          seleccionDateTimeDesde(initialDateController,
-           initialDate,
-           initialTimeOfDayController, 
-           initialTimeOfDay,
-           "Desde",
-           "Desde"),
-         /*  seleccionDateTimeDesde(
-            endDateController, 
-            endDate, 
-            endTimeOfDayController, 
-            endTimeOfDay,
-            "Hasta",
-            "Hasta"), */
-          ButtonBar(
-            children: <Widget>[
-              FlatButton(
-                onPressed: goBack,
-                child: Text('Cancelar')
-              ),
-              RaisedButton( 
-                onPressed: enviar,
-                child: Text('Enviar')
+              labelText: "Direccion"),
+          onTap: getGeo,
+          controller: directionTextController,
+        ),
+        seleccionDateTimeDesde(initialDateController, initialDate,
+            initialTimeOfDayController, initialTimeOfDay, "Desde", "Desde"),
+        TextFormField(
+          decoration: InputDecoration(labelText: "Comentario"),
+          keyboardType: TextInputType.multiline,
+          maxLines: null,
+          minLines: 2,
+        ),
+        Row(children: [
+          Expanded(
+            child: RaisedButton(
+              onPressed: openAgregarImageDialog,
+              color: Theme.of(context).primaryColor,
+              child: Container(
+                child:Text("Agregar Imagen"),
               )
-            ]
+            )
           )
-        ]
-      ) ,
+        ]),
+        ButtonBar(children: <Widget>[
+          FlatButton(onPressed: goBack, child: Text('Cancelar')),
+          RaisedButton(onPressed: enviar, child: Text('Enviar'))
+        ])
+      ]),
     );
   }
 
-  Future<void> checkDateDate(DateTime date, TextEditingController controller) async {
+  void openAgregarImageDialog() {}
+
+  Future<void> checkDateDate(
+      DateTime date, TextEditingController controller) async {
     final result = await openDataPicker();
-    if( result != null){
+    if (result != null) {
       setState(() {
         date = result;
-        controller.value = controller.value.copyWith(
-          text: dateText(date)
-        );
+        controller.value = controller.value.copyWith(text: dateText(date));
       });
     }
   }
 
-  void goBack(){
+  void goBack() {
     Navigator.pop(context);
   }
 
-  Future<void> enviar() async{
+  Future<void> enviar() async {
     raiseLoadingModal();
     assert(initialTimeOfDay != null);
     var ticket = Ticket();
@@ -151,38 +118,42 @@ class _NewTicketFormState extends State<NewTicketForm> {
     //Navigator.pop(context);
   }
 
-  Future<DateTime> openDataPicker() async{
+  Future<DateTime> openDataPicker() async {
     return showDatePicker(
-      context: context, 
-      initialDate: DateTime.now(),
-      firstDate: DateTime.now(),
-      lastDate: DateTime.utc(2055,2,20),
-      initialDatePickerMode: DatePickerMode.day
-      );
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime.now(),
+        lastDate: DateTime.utc(2055, 2, 20),
+        initialDatePickerMode: DatePickerMode.day);
   }
 
-  Future<TimeOfDay> openHourPicker() async{
-    return showTimePicker(context: context, initialTime: TimeOfDay(hour: 12, minute: 0));
+  Future<TimeOfDay> openHourPicker() async {
+    return showTimePicker(
+        context: context, initialTime: TimeOfDay(hour: 12, minute: 0));
   }
 
-  Future<void> checkHour(TimeOfDay timeOfDay, TextEditingController controller) async{
+  Future<void> checkHour(
+      TimeOfDay timeOfDay, TextEditingController controller) async {
     final result = await openHourPicker();
-    if(result != null){
-      setState((){
-        timeOfDay = timeOfDay.replacing(hour: result.hour, minute: result.minute);
-        controller.value = controller.value.copyWith(text: timeOfDay.hour.toString() + ":" + timeOfDay.minute.toString());
+    if (result != null) {
+      setState(() {
+        timeOfDay =
+            timeOfDay.replacing(hour: result.hour, minute: result.minute);
+        controller.value = controller.value.copyWith(
+            text:
+                timeOfDay.hour.toString() + ":" + timeOfDay.minute.toString());
       });
     }
   }
 
   Widget seleccionDateTimeDesde(
-    TextEditingController datecontroller, 
+    TextEditingController datecontroller,
     DateTime dateTime,
-    TextEditingController hourController, 
+    TextEditingController hourController,
     TimeOfDay timeOfDay,
     String hintText,
     String labelText,
-    ){
+  ) {
     return Row(
       children: <Widget>[
         Container(
@@ -191,11 +162,12 @@ class _NewTicketFormState extends State<NewTicketForm> {
           ),
           child: TextFormField(
             decoration: InputDecoration(
-              icon: Icon(Icons.calendar_today),
-              hintText: hintText,  
-              labelText: labelText
-            ),
-            onTap: () { checkDateDate(dateTime, datecontroller);},
+                icon: Icon(Icons.calendar_today),
+                hintText: hintText,
+                labelText: labelText),
+            onTap: () {
+              checkDateDate(dateTime, datecontroller);
+            },
             controller: datecontroller,
           ),
         ),
@@ -204,51 +176,53 @@ class _NewTicketFormState extends State<NewTicketForm> {
             maxWidth: 150,
           ),
           child: TextFormField(
-            onTap: (){ checkHour(timeOfDay, hourController);},
-            controller: hourController,
-            decoration: InputDecoration(
-              hintText: "Hora",
-              labelText: "Hora",
-              icon: Icon(Icons.timer),
-            )
-          ),
+              onTap: () {
+                checkHour(timeOfDay, hourController);
+              },
+              controller: hourController,
+              decoration: InputDecoration(
+                hintText: "Hora",
+                labelText: "Hora",
+                icon: Icon(Icons.timer),
+              )),
         )
       ],
     );
   }
 
-  void raiseLoadingModal(){
+  void raiseLoadingModal() {
     showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return Utilities.loadingAlert();
-      }  
-      );
+        context: context,
+        builder: (BuildContext context) {
+          return Utilities.loadingAlert();
+        });
   }
 
-  void raiseSuccessModal(){
+  void raiseSuccessModal() {
     showDialog(
       context: context,
       builder: (context) => Utilities.successAlert(() {
-        Navigator.pop(context); 
         Navigator.pop(context);
-        }),
+        Navigator.pop(context);
+      }),
       barrierDismissible: false,
     );
   }
-
-  
 
   Future<void> getGeo() async {
     GPSLocation loc = GPSLocation();
     raiseLoadingModal();
     try {
       var geo = Geolocator();
-      var q = await geo.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+      var q =
+          await geo.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
       print(q);
       loc.longitud = q.longitude;
       loc.latitud = q.latitude;
-      loc.direccion = 'Placeholder: ' + loc.longitud.toString() + " " + loc.latitud.toString();
+      loc.direccion = 'Placeholder: ' +
+          loc.longitud.toString() +
+          " " +
+          loc.latitud.toString();
     } catch (e) {
       print(e);
       loc.latitud = 0;
@@ -257,6 +231,7 @@ class _NewTicketFormState extends State<NewTicketForm> {
     }
     location = loc;
     Navigator.pop(context);
-    directionTextController.value = directionTextController.value.copyWith(text: loc.direccion);
+    directionTextController.value =
+        directionTextController.value.copyWith(text: loc.direccion);
   }
 }
