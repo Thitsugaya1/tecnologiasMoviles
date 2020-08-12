c3978afbc1ec45a33973a86caa93d73d1b2b6a8e
gbunny <gbunny@protonmail.com>
2020-08-12 11:58:19 -0400

    Mic Audio

---
+++ flutterApp/lib/ui/common/AudioPlayer.dart
@@ -0,0 +1,148 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart' as players;
+
+
+class AudioPlayer extends StatefulWidget {
+  final String audiopath;
+  AudioPlayer( this.audiopath, {Key key}) : super(key: key);
+
+  @override
+  _AudioPlayerState createState() => _AudioPlayerState();
+}
+
+class _AudioPlayerState extends State<AudioPlayer> {
+  players.AudioPlayer _player;
+  players.AudioPlayerState _playerState = players.AudioPlayerState.STOPPED;
+  Duration _duration;
+  Duration _position;
+
+  StreamSubscription _durationSubscription;
+  StreamSubscription _positionSubscription;
+  StreamSubscription _playerStateSubscription;
+  StreamSubscription _playerErrorSubcription;
+  StreamSubscription _playercompleteSubscription;
+
+  get _durationText => _duration?.toString()?.split('.')?.first ?? '';
+  get _positionText => _position?.toString()?.split('.')?.first ?? '';
+  get _isPlaying => (_playerState == players.AudioPlayerState.PLAYING);
+  get _isPaused => (_playerState == players.AudioPlayerState.PAUSED);
+
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  void _init() async {
+    this._player = players.AudioPlayer(mode: players.PlayerMode.MEDIA_PLAYER);
+    _durationSubscription = _player.onDurationChanged.listen((event) {
+      setState(()=> _duration = event);
+    });
+    _positionSubscription = _player.onAudioPositionChanged.listen((event) {
+      setState(()=> _position = event);
+    });
+    _playerErrorSubcription = _player.onPlayerError.listen((event) {
+      print('Audio Error $event');
+      setState(() {
+        _playerState = players.AudioPlayerState.STOPPED;
+        _duration = Duration(seconds: 0);
+        _position = Duration(seconds: 0);
+      });
+    });
+    _playercompleteSubscription = _player.onPlayerCompletion.listen((event) {
+      setState((){
+        _position = _duration;
+      });
+    });
+    _playerStateSubscription = _player.onPlayerStateChanged.listen((event) {
+      if(!mounted) return;
+      setState((){
+        _playerState = event;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _player.dispose();
+    _playercompleteSubscription.cancel();
+    _playerErrorSubcription.cancel();
+    _playerStateSubscription.cancel();
+    _durationSubscription.cancel();
+    _positionSubscription.cancel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              icon: Icon(Icons.play_circle_outline),
+              onPressed: _isPlaying ? null : _play,
+            ),
+            IconButton(
+              icon: Icon(Icons.pause_circle_outline),
+              onPressed: _isPlaying ? _pause : null,
+            ),
+            IconButton(
+              icon: Icon(Icons.stop),
+              onPressed: (_isPlaying || _isPaused) ? _stop : null,
+            ),
+          ]
+        ,),
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(12),
+              child: Stack(
+                children: [
+                  Slider(
+                    onChanged: (v) {
+                      Duration position = Duration(milliseconds: (v * _duration.inMilliseconds).round());
+                      _player.seek(position);
+                    },
+                    value: (_position != null && 
+                            _duration != null && 
+                            _position.inMilliseconds > 0 && 
+                            _position < _duration ) ? _position.inMilliseconds / _duration.inMilliseconds : 0,
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              (_position != null) ? '$_positionText / $_durationText' : _durationText,
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+ 
+  _play() async{
+    final playPosition  = ( _position != null && _duration != null &&
+    _position.inMilliseconds > 0 && _position.inMilliseconds < _duration.inMilliseconds) ? _position : null;
+    await _player.play(widget.audiopath, isLocal: true, position: playPosition);
+  }
+
+  _pause() async{
+    await _player.pause();
+  }
+
+  _stop() async{
+    final result = await _player.stop();
+    if (result == 1){
+      setState(() {
+        _position = Duration();
+      });
+    }
+  }
+}
---
+++ flutterApp/lib/ui/common/RecordAudio.dart
@@ -0,0 +1,195 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_audio_recorder/flutter_audio_recorder.dart';
+
+import 'package:path/path.dart' as Path;
+import 'package:path_provider/path_provider.dart';
+import 'package:ticketapp/ui/common/AudioPlayer.dart';
+
+
+class RecordAudioWidget extends StatefulWidget {
+  RecordAudioWidget({Key key}) : super(key: key);
+
+  @override
+  _RecordAudioWidgetState createState() => _RecordAudioWidgetState();
+}
+
+class _RecordAudioWidgetState extends State<RecordAudioWidget> {
+  FlutterAudioRecorder _recorder;
+  RecordingStatus _recordingStatus = RecordingStatus.Unset;
+  Recording _current;
+  Timer _t;
+
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _recorder.stop();
+    _t.cancel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.all(5),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              startButton(context),
+              pauseButton(context),
+              stopButton(context)
+              ],),
+              player(context),
+              bottomBar(context),
+          ],
+        ) ,
+      )
+    );
+  }
+
+  Widget startButton(BuildContext context){
+    return (_recordingStatus == RecordingStatus.Initialized || _recordingStatus == RecordingStatus.Stopped) ? 
+      RaisedButton(child: Icon(Icons.mic), color: Colors.redAccent, onPressed: _record,):
+      RaisedButton(child: Icon(Icons.mic), disabledColor: Colors.grey, onPressed: null);
+  }
+
+  Widget pauseButton(BuildContext context){
+    switch (_recordingStatus) {
+      case RecordingStatus.Recording:
+        return RaisedButton(child: Icon(Icons.pause), onPressed: _pause);
+      case RecordingStatus.Paused:
+        return RaisedButton(child: Icon(Icons.play_arrow), onPressed: _resume);
+      default:
+        return RaisedButton(child: Icon(Icons.pause), 
+        onPressed: null, 
+        disabledColor: Color.alphaBlend(Theme.of(context).primaryColor.withAlpha(60), Colors.grey),
+        );
+    }
+  }
+
+  Widget stopButton(BuildContext context){
+    if(_recordingStatus == RecordingStatus.Recording || _recordingStatus == RecordingStatus.Paused){
+      return RaisedButton(child: Icon(Icons.stop), onPressed: _stop, color: Colors.red);
+    }
+    else{
+      return RaisedButton(child: Icon(Icons.stop), onPressed: null, disabledColor: Color.alphaBlend(Colors.red.withAlpha(60), Colors.grey));
+    }
+  }
+
+  Future<int> _init() async{
+    try{
+      if (await FlutterAudioRecorder.hasPermissions){
+        final path = Path.join(
+          (await getTemporaryDirectory()).path, '${DateTime.now().millisecondsSinceEpoch}.m4a');
+        _recorder = FlutterAudioRecorder(path);
+
+        await _recorder.initialized;
+
+        var current = await _recorder.current(channel: 0);
+        print(current);
+        setState(() {
+          _current = current;
+          _recordingStatus = current.status;
+          print(_recordingStatus);
+        });
+      }
+    }catch(e){
+      print(e);
+    }
+    return 1;
+  }
+
+
+  void _stop() async {
+    var result = await _recorder.stop();
+    print('stop recording: ${result.path} :: ${result.duration}');
+    setState(() {
+      _current =result;
+      _recordingStatus = _current.status;
+    });
+  }
+
+  void _resume() async {
+    await _recorder.resume();
+    setState(() {
+      _recordingStatus = _current.status;
+    });
+  }
+
+  void _pause() async {
+    await _recorder.pause();
+    setState(() {
+      _recordingStatus = _current.status;
+    });
+  }
+
+  void _record() async {
+    if(_recordingStatus == RecordingStatus.Stopped){
+      _recordingStatus = RecordingStatus.Unset;
+      //await File(_current.path).delete();
+      _t.cancel();
+      await _init();
+    }
+    try{
+      await _recorder.start();
+      var recording = await _recorder.current(channel:0);
+      setState(() {
+        _current = recording;
+        _recordingStatus = _current.status;
+      });
+
+      const tick = const Duration(milliseconds: 50);
+      _t = new Timer.periodic(tick, _tickEvent);
+    }catch(e){
+      print(e);
+    }
+  }
+
+  void _tickEvent(Timer t) async {
+    if(_recordingStatus == RecordingStatus.Stopped){
+      t.cancel();
+    }
+    var current = await _recorder.current(channel: 0);
+    setState(() {
+      _current = current;
+      _recordingStatus = _current.status;
+    });
+  }
+
+  Widget player(BuildContext context) {
+    if(_recordingStatus == RecordingStatus.Stopped){
+      return AudioPlayer(_current.path);
+    } else return Container(
+      child: Text('${_current?.duration ?? ' '}')
+    );
+  }
+
+  Widget bottomBar(BuildContext context){
+    return ButtonBar(
+      children: [
+        FlatButton(child: Text("Cancel"), onPressed: (){
+          Navigator.pop(context);
+        },),
+        RaisedButton(
+          child: Text("Aceptar"),
+          onPressed: (_recordingStatus == RecordingStatus.Stopped) ? (){
+            Navigator.pop(context, _current.path);
+          } : null,
+          disabledColor: Color.alphaBlend(Theme.of(context).primaryColor.withAlpha(70), Colors.grey),
+        )
+      ],);
+  }
+
+}
